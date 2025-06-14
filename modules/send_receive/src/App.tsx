dd2837ab81a299cbc27591bc64937f802bbdd103
🧃hyper ririi <plush@fergalicious>
2025-06-14 03:03:27 -0400

    basic react app.. basic monorepo structure

---
+++ modules/send_receive/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>desk_pipe</h1>
+      </div>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
